9bbaee81befeae7a7c2a1b53183380e025bc749a
BiiN12 <biniyameshete12@gmail.com>
2024-12-03 22:00:09 +0200

    New

--- javascript.js
+++ javascript.js
@@ -1,19 +1,26 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomImgSrc1 = "/images/dice" + randomNumber1 + ".png";
-var img1 = document
-  .querySelectorAll("img")[0]
-  .setAttribute("src", randomImgSrc1);
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImgSrc1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelectorAll("img")[0].setAttribute("src", randomImgSrc1);
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImgSrc2 = "/images/dice" + randomNumber2 + ".png";
-var img2 = document
-  .querySelectorAll("img")[1]
-  .setAttribute("src", randomImgSrc2);
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImgSrc2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImgSrc2);
 
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
-} else {
-  document.querySelector("h1").innerHTML = "Draw";
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw";
+  }
 }
+
+window.onload = function () {
+  const navigationEntries = performance.getEntriesByType("navigation")[0];
+
+  if (navigationEntries && navigationEntries.type === "reload") {
+    // Run the dice game only on refresh
+    rollDice();
+  }
+};
